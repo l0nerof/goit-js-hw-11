ae0957518e2a3cc7791207e7c628f871f6b52b4c
Volodymyr Bondar <2005volka@gmail.com>
2023-05-01 18:36:58 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,110 @@
+import './css/styles.css';
+import { Notify } from 'notiflix';
+import axios from 'axios';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+const apiKey = '35821051-ff4ed7cb6cca304ad1608b196';
+const perPage = 40;
+let currentPage = 1;
+let currentQuery = '';
+
+refs.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const query = e.target.searchQuery.value.trim();
+  if (query === '') return;
+
+  currentQuery = query;
+  currentPage = 1;
+  fetchImages(currentQuery, currentPage);
+});
+
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+async function fetchImages(query, page) {
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`;
+  try {
+    const response = await axios.get(url);
+    const data = await response.data;
+    if (data.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    refs.loadMoreBtn.classList.remove('hide');
+    renderGallery(data.hits);
+  } catch (error) {
+    Notify.failure('Oops! Something went wrong. Please try again later.');
+    console.log(error);
+  }
+}
+
+function renderGallery(images) {
+  if (currentPage === 1) {
+    refs.gallery.innerHTML = '';
+  }
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `
+  <div class="photo-card">
+    <a class="photo-link" href="${largeImageURL}">
+      <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+    </a>
+      <div class="info">
+     <p class="info-item">
+        <b>Likes</b>${likes}
+      </p>
+     <p class="info-item">
+        <b>Views</b>${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>${comments}
+     </p>
+      <p class="info-item">
+       <b>Downloads</b>${downloads}
+     </p>
+  </div>
+</div>
+`
+    )
+    .join('');
+  return (refs.gallery.innerHTML = markup);
+}
+
+async function onLoadMore() {
+  currentPage += 1;
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${apiKey}&q=${currentQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${currentPage}`
+    );
+    const data = response.data;
+    if (data.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no more images matching your search query.'
+      );
+      return;
+    }
+    renderGallery(data.hits);
+    refs.gallery.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  } catch (error) {
+    Notify.failure('Oops! Something went wrong. Please try again later.');
+    console.log(error);
+  }
+}
